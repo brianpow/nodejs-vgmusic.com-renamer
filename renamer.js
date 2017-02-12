cc8a3d86f5bd4a1b3272bb9971c372f4ff8dbb9e
Brian Pow <brianpow@gmail.com>
2017-02-12 21:38:35 +0800

    add switch to copy file instread of linking file, add switch to search path, add 'quiet' and 'verbose' switch

--- renamer.js
+++ renamer.js
@@ -6,88 +6,120 @@ var sanitize = require("sanitize-filename")
 var FindFiles = require("node-find-files")
 var fs = require("fs")
 var mkdirp = require("mkdirp")
+var program = require("commander")
+var shellEscape = require("shell-escape")
 
-if (process.argv.length < 4) {
-    console.error("Usage: %s %s <source path> <target path>", process.argv[0], process.argv[1])
-    process.exit(1)
-}
-var source = Path.resolve(process.argv[2])
-var target = Path.resolve(process.argv[3])
-var finder = new FindFiles({
-    rootFolder: source,
-    filterFunction: function(path, stat) {
-        if (path.indexOf("index.html") != -1) {
-            var files = fs.readdirSync(Path.dirname(path))
-            for (var file of files)
-                if (file.substr(-4).toLowerCase() == ".mid")
-                    return true
-        }
-        return false
-    }
-}).on("match",
-    function(path, stat) {
-        console.log("Processing " + path)
+program.version("1.1.0")
+    .option('-q, --quiet', 'be quiet (except error message)')
+    .option('-v, --verbose', 'be verbose', increaseVerbosity, 0)
+    .option('-s, --search <keyword>', 'process only path contain keyword')
+    .option('-c, --copy', 'copy instead of symobolic linking the original file')
+    .option('-e, --export <file>', 'export the process in form of shell script instead of direct linking/copying')
+    .action(function(source, target) {
+        var finder = new FindFiles({
+            rootFolder: source,
+            filterFunction: function(path, stat) {
+                if (!program.search || path.indexOf(program.search) != -1)
+                    if (path.indexOf("index.html") != -1) {
+                        var files = fs.readdirSync(Path.dirname(path))
+                        for (var file of files)
+                            if (file.substr(-4).toLowerCase() == ".mid")
+                                return true
+                    }
+                return false
+            }
+        }).on("match",
+            function(path, stat) {
+                if (!program.quiet)
+                    console.log("Processing " + path)
 
-        var data = fs.readFileSync(path, 'utf8')
-        var current = Path.dirname(path)
-        try {
-            var lastTitle = ""
-            var files = parse(data)
-            for (var i = 0; i < files.length; i++) {
-                var file = files[i]
-
-                var newTargetPath = Path.join(target, current.substr(source.length + 1), file[0])
-                if (lastTitle != file[0]) {
-                    console.log("mkdirp %s", newTargetPath)
-                    mkdirp.sync(newTargetPath)
-                    lastTitle = file[0]
-                }
-                var author = (file[2] == "") ? "" : " [" + file[2] + "]"
-
-                var newName = file[1] + author + ".mid"
-                newName = sanitize(newName)
-
-                var originalName = unescape(file[3].replace(/.+\//, ""))
-                var fullSource = Path.join(current, originalName)
-                var fullTarget = Path.join(newTargetPath, newName)
+                var data = fs.readFileSync(path, 'utf8')
+                var current = Path.dirname(path)
                 try {
-                    var stat = fs.lstatSync(fullTarget)
-                    if (!stat.isFile() && !stat.isSymbolicLink()) {
-                        try {
-                            console.log("Linking %s to %s", fullSource, fullTarget)
-                            fs.symlinkSync(fullSource, fullTarget)
-                        } catch (e) {
-                            console.dir(e)
-                            process.exit(1)
+                    var lastTitle = ""
+                    var files = parse(data)
+                    for (var i = 0; i < files.length; i++) {
+                        var file = files[i]
+
+                        var newTargetPath = Path.join(target, current.substr(source.length), file[0])
+                        if (lastTitle != file[0] && !fs.existsSync(newTargetPath)) {
+                            if (!program.quiet && program.verbose > 0)
+                                console.log("mkdirp %s", newTargetPath)
+                            mkdirp.sync(newTargetPath)
+                            lastTitle = file[0]
                         }
-                    } else {
-                        console.log("Skipping %s to %s", fullSource, fullTarget)
+                        var author = (file[2] == "") ? "" : " [" + file[2] + "]"
+
+                        var newName = file[1] + author + ".mid"
+                        newName = sanitize(newName)
+
+                        var originalName = unescape(file[3].replace(/.+\//, ""))
+                        var fullSource = Path.join(current, originalName)
+                        var fullTarget = Path.join(newTargetPath, newName)
+                        try {
+                            var stat = fs.lstatSync(fullTarget)
+                            if (!stat.isFile() && !stat.isSymbolicLink()) {
+                                try {
+                                    link(fullSource, fullTarget)
+
+                                } catch (e) {
+                                    console.dir(e)
+                                    process.exit(1)
+                                }
+                            } else {
+                                if (!program.quiet && program.verbose > 0)
+                                    console.log("Skipping %s to %s", fullSource, fullTarget)
+                            }
+                        } catch (e) {
+                            link(fullSource, fullTarget)
+                        }
                     }
                 } catch (e) {
-                    console.log("Linking %s to %s", fullSource, fullTarget)
-                    fs.symlinkSync(fullSource, fullTarget)
+                    console.error(e)
+                    process.exit(1)
                 }
+            }).on("complete", function() {
+            if (!program.quiet)
+                console.log("Finished")
+        })
+        finder.startSearch()
+    })
+    .parse(process.argv)
 
-            }
-        } catch (e) {
-            console.error(e)
-            process.exit(1)
+function increaseVerbosity(v, total) {
+    return total + 1;
+}
+
+function link(fullSource, fullTarget) {
+    if (program.copy) {
+        if (!program.export) {
+            if (!program.quiet && program.verbose > 0)
+                console.log("Copying %s to %s", fullSource, fullTarget)
+            fs.createReadStream(fullSource).pipe(fs.createWriteStream(fullTarget))
+        } else {
+            fs.appendFileSync(program.export, shellEscape(['cp', fullSource, fullTarget]))
         }
-    }).on("complete", function() {
-    console.log("Finished")
-})
+    } else {
+        if (!program.export) {
+            if (!program.quiet && program.verbose > 0)
+                console.log("Linking %s to %s", fullSource, fullTarget)
+            fs.symlinkSync(fullSource, fullTarget)
+        } else {
+            console.log(shellEscape(['ln', '-s', fullSource, fullTarget]))
+            fs.appendFileSync(program.export, shellEscape(['ln', '-s', fullSource, fullTarget]))
+        }
+    }
+}
 
 function parse(data) {
     var files = []
     var $ = cheerio.load(data)
     var $trs = $("tr")
-
     var title = ""
     for (var i = 2; i < $trs.length; i++) {
         var $tr = $($trs[i])
         if ($tr.hasClass("header")) {
             title = sanitize($tr.text().trim())
-
         } else {
             var $td = $tr.find("td")
             var a = $td.eq(0).find("a").eq(0)
@@ -100,5 +132,3 @@ function parse(data) {
     }
     return files
 }
-
-finder.startSearch()
